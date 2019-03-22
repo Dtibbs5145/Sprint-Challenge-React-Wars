57a49e0bb2a9ef3b43a69ed24642333ab96f3700
Destiny Tibbitts <dtibbs5145@gmail.com>
2019-03-22 15:56:35 -0400

    added a background to the character cards

--- starwars/src/components/StarwarsChar.js
+++ starwars/src/components/StarwarsChar.js
@@ -7,19 +7,13 @@ const StarWarsChar = props => {
                 <div className='details'>
                     <h2>{props.char.name}</h2>
                     <p><strong>Birth Year:</strong>{props.char.birth_year}</p>
-                    <p><strong>Home World:</strong> <img src='{props.char.homeworld}' alt="home world" /></p>
                     <p><strong>Height:</strong>{props.char.height}</p>
                     <p><strong>Gender:</strong>{props.char.gender}</p>
-                    <p><strong>Species:</strong> <img src='{props.char.species}' alt='species' /></p>
                     <p><strong>Created:</strong>{props.char.created}</p>
                     <p><strong>Edited:</strong>{props.char.edited}</p>
                     <p><strong>Eye Color:</strong>{props.char.eye_color}</p>
-                    <p><strong>Films:</strong> <img src='{props.char.films}' alt='films' /></p>
-                    <p><strong>Hair Color:</strong> <img src='{props.char.hair_color}' alt="hair color" /></p>
                     <p><strong>Mass:</strong>{props.char.mass}</p>
                     <p><strong>Skin Color:</strong>{props.char.skin_color}</p>
-                    <p><strong>Starship:</strong>{props.char.starship}</p>
-                    <p><strong>Vehicles:</strong> <img src='{props.char.vehicles}' alt='vehicles' /></p>
                 </div>
             </div>
         </div>
